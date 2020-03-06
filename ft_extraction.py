7b1db7e61a1af5ae5fd0d55aa3260840f72b7037
QUANG TRAN <quangtran0698@gmail.com>
2020-03-06 16:22:20 -0300

    add files

---
+++ ft_extraction.py
@@ -0,0 +1,19 @@
+# adapted from https://medium.com/@franky07724_57962/using-keras-pre-trained-models-for-feature-extraction-in-image-clustering-a142c6cdf5b1
+from keras.preprocessing import image
+from keras.applications.vgg16 import VGG16
+from keras.applications.vgg16 import preprocess_input
+import numpy as np
+
+model = VGG16(weights='imagenet', include_top=False)
+model.summary()
+
+img_path = './airport_crop/0.png'
+img = image.load_img(img_path, target_size=(224, 224))
+img_data = image.img_to_array(img)
+img_data = np.expand_dims(img_data, axis=0)
+img_data = preprocess_input(img_data)
+
+vgg16_feature = model.predict(img_data)
+vgg16_feature = np.ndarray.flatten(vgg16_feature)
+
+print(vgg16_feature.shape)
